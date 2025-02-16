97bf491bfd3a81a5796dea6c4f038b9f7df950ef
Jayakumar M <132869362+jaidhanush@users.noreply.github.com>
2025-02-16 20:03:16 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -213,4 +213,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'jaidhanushdh@gmail.com'  # Replace with your Gmail
-EMAIL_HOST_PASSWORD = 'iyzh aqye txmc mqqj'  # Use the App Password, NOT your actual password
+EMAIL_HOST_PASSWORD = 'iyzh aq txmc mqqj'  # Use the App Password, NOT your actual password
